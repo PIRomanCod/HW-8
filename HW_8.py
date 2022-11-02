ea144bf67b567a7969d3e0207afca2891578bf21
Roman Pimonov <111997187+PIRomanCod@users.noreply.github.com>
2022-11-02 20:10:48 +0200

    Update HW_8.py

--- HW_8.py
+++ HW_8.py
@@ -15,24 +15,24 @@ from datetime import datetime, timedelta, date
 from collections import defaultdict
 
 
-users = [{"Bill": datetime(year=2012, month=11, day=2)},
-         {"Oleg": datetime(year=2009, month=11, day=2)},
-         {"Jill": datetime(year=2009, month=11, day=3)},
-         {"Vasyl": datetime(year=2009, month=11, day=3)},
-         {"Kim": datetime(year=2001, month=11, day=4)},
-         {"Nikolay": datetime(year=2001, month=11, day=4)},
-         {"Jan": datetime(year=1997, month=11, day=5)},
-         {"Petro": datetime(year=1999, month=11, day=5)},
-         {"Jack": datetime(year=1980, month=11, day=6)},
-         {"Mykola": datetime(year=1985, month=11, day=6)},
-         {"Will": datetime(year=1981, month=11, day=7)},
-         {"Smith": datetime(year=1990, month=11, day=7)},
-         {"Gil": datetime(year=1991, month=11, day=8)},
-         {"Gul": datetime(year=1995, month=11, day=8)},
-         {"Murat": datetime(year=1998, month=11, day=9)},
-         {"Lola": datetime(year=1970, month=11, day=10)},
-         {"Kola": datetime(year=1975, month=11, day=11)},
-         {"Nika": datetime(year=1987, month=11, day=23)}]
+users = [{"name": "Bill", "birthday": datetime(year=2012, month=11, day=2)},
+         {"name": "Oleg", "birthday": datetime(year=2009, month=11, day=2)},
+         {"name": "Jill", "birthday": datetime(year=2009, month=11, day=3)},
+         {"name": "Vasyl", "birthday": datetime(year=2009, month=11, day=3)},
+         {"name": "Kim", "birthday": datetime(year=2001, month=11, day=4)},
+         {"name": "Nikolay", "birthday": datetime(year=2001, month=11, day=4)},
+         {"name": "Jan", "birthday": datetime(year=1997, month=11, day=5)},
+         {"name": "Petro", "birthday": datetime(year=1999, month=11, day=5)},
+         {"name": "Jack", "birthday": datetime(year=1980, month=11, day=6)},
+         {"name": "Mykola", "birthday": datetime(year=1985, month=11, day=6)},
+         {"name": "Will", "birthday": datetime(year=1981, month=11, day=7)},
+         {"name": "Smith", "birthday": datetime(year=1990, month=11, day=7)},
+         {"name": "Gil", "birthday": datetime(year=1991, month=11, day=8)},
+         {"name": "Gul", "birthday": datetime(year=1995, month=11, day=8)},
+         {"name": "Murat", "birthday": datetime(year=1998, month=11, day=9)},
+         {"name": "Lola", "birthday": datetime(year=1970, month=11, day=10)},
+         {"name": "Kola", "birthday": datetime(year=1975, month=11, day=11)},
+         {"name": "Nika", "birthday": datetime(year=1987, month=11, day=23)}]
 
 weekdays = ["Monday", "Tuesday", "Wednesday",
             "Thursday", "Friday", "Saturday", "Sunday"]
@@ -46,16 +46,19 @@ def get_birthdays_per_week(users, weekdays) -> dict:
                             month=current_datetime.month, day=current_datetime.day+1)
     one_weeks_interval = timedelta(days=6)
     end_of_view = current_datetime + one_weeks_interval
-    print(current_datetime, end_of_view)
-    for item in users:
-        for key, value in item.items():
-            value = value.date()
-            if current_datetime.month <= value.month <= end_of_view.month:
-                if current_datetime.day <= value.day <= end_of_view.day:
-                    new_date = date(year=current_datetime.year,
-                                    month=value.month, day=value.day).weekday()
-                    list_of_birthdays.append([weekdays[new_date], key])
-    grouped_words = defaultdict(list)
+
+    for user in users:
+        new_date_for_user = date(
+            year=current_datetime.year,
+            month=user.get('birthday').month,
+            day=user.get('birthday').day)
+
+        if current_datetime <= new_date_for_user <= end_of_view:
+            new_date = new_date_for_user.weekday()
+            if new_date in (5, 6):
+                new_date = 0
+            list_of_birthdays.append([weekdays[new_date], user.get("name")])
+        grouped_words = defaultdict(list)
 
     for word in list_of_birthdays:
         char = word[0]
@@ -66,4 +69,5 @@ def get_birthdays_per_week(users, weekdays) -> dict:
         print(f"{key}: {', '.join(value)}")
 
 
-get_birthdays_per_week(users, weekdays)
+if __name__ == '__main__':
+    get_birthdays_per_week(users, weekdays)
